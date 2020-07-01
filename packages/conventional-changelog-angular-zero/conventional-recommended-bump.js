d80ca7cf67d7b4f296806ccd3a2c7916f57959cf
Jamie Rolfs <jamie.rolfs@gmail.com>
2020-07-01 15:18:31 -0700

    chore(preset:angular-zero): fix linting

--- packages/conventional-changelog-angular-zero/conventional-recommended-bump.js
+++ packages/conventional-changelog-angular-zero/conventional-recommended-bump.js
@@ -1,43 +1,43 @@
-'use strict';
+'use strict'
 
-const parserOpts = require('./parser-opts');
-const readPkgUp = require('read-pkg-up');
+const parserOpts = require('./parser-opts')
+const readPkgUp = require('read-pkg-up')
 
 const {
-  packageJson: { noMajorBump },
-} = readPkgUp.sync();
+  packageJson: { noMajorBump }
+} = readPkgUp.sync()
 
 module.exports = {
   parserOpts,
 
   whatBump: commits => {
-    let level = 2;
-    let breakings = 0;
-    let features = 0;
+    let level = 2
+    let breakings = 0
+    let features = 0
 
     commits.forEach(commit => {
       if (commit.notes.length > 0) {
-        breakings += commit.notes.length;
+        breakings += commit.notes.length
 
         if (Array.isArray(noMajorBump) && noMajorBump.includes(commit.scope)) {
-          level = 1;
+          level = 1
         } else {
-          level = 0;
+          level = 0
         }
       } else if (commit.type === 'feat') {
-        features += 1;
+        features += 1
         if (level === 2) {
-          level = 1;
+          level = 1
         }
       }
-    });
+    })
 
     return {
       level: level,
       reason:
         breakings === 1
           ? `There is ${breakings} BREAKING CHANGE and ${features} features`
-          : `There are ${breakings} BREAKING CHANGES and ${features} features`,
-    };
-  },
-};
+          : `There are ${breakings} BREAKING CHANGES and ${features} features`
+    }
+  }
+}
